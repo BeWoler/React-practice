77fb3b7d2b1fa81f34702e258d80127e7afee2c1
BeWoler <bewoler.del@gmail.com>
2021-08-30 14:04:06 +0300

    added class counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <div className="component__box">
+        <h2>Functional component</h2>
+        <Counter />
+      </div>
+      <div className="component__box">
+        <h2>Class component</h2>
+        <ClassCounter />
+      </div>
     </div>
   );
 }
