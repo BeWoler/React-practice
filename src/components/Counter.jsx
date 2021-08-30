1c91ad9024d2e8b3415bac88027cb6cb7c4cd0c9
BeWoler <bewoler.del@gmail.com>
2021-08-30 13:51:00 +0300

    rename counter

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,24 +1,28 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 export default function Counter() {
-  const [likes, setLikes] = useState(0);
-  const [value, setValue] = useState('Hello');
+  const [count, setCount] = useState(0);
+  const [value, setValue] = useState("Hello");
 
   function increment() {
-    setLikes(likes + 1);
+    setCount(count + 1);
   }
 
   function decrement() {
-    setLikes(likes - 1);
+    setCount(count - 1);
   }
 
-  return(
+  return (
     <div>
-      <h1>{likes}</h1>
+      <h1>{count}</h1>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
       <h2>{value}</h2>
-      <input type="text" value={value} onChange={event => setValue(event.target.value)}/>
+      <input
+        type="text"
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+      />
     </div>
-  )
+  );
 }
