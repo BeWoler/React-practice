f0156bf5a748baf91f4914835139ab01ac7e5094
BeWoler <bewoler.del@gmail.com>
2021-08-31 18:16:51 +0300

    added PostForm component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import { useState } from "react";
 import "./App.css";
 import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
+import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
 
 function App() {
-  const [post, setPost] = useState({ title: "", body: "" });
   const [posts, setPosts] = useState([
     { id: 1, title: "JavaScript", body: "Description" },
     { id: 2, title: "JavaScript 2", body: "Description" },
@@ -15,11 +13,10 @@ function App() {
     { id: 4, title: "JavaScript 4", body: "Description" },
   ]);
 
-  const addNewPost = (e) => {
-    e.preventDefault();
-    setPosts([...posts, { ...post, id: Date.now() }]);
-    setPost({ title: "", body: "" });
-  };
+   const createPost = newPost => {
+     setPosts([...posts, newPost ])
+   }
+
   return (
     <div className="App">
       <div className="component__box">
@@ -31,26 +28,7 @@ function App() {
         <ClassCounter />
       </div>
 
-      <form>
-        <MyInput
-          value={post.title}
-          type="text"
-          placeholder="Post title"
-          onChange={(e) => {
-            setPost({ ...post, title: e.target.value });
-          }}
-        />
-        <MyInput
-          value={post.body}
-          type="text"
-          placeholder="Post body"
-          onChange={(e) => {
-            setPost({ ...post, body: e.target.value });
-          }}
-        />
-        <MyButton onClick={addNewPost}>Создать пост</MyButton>
-      </form>
-
+      <PostForm create={createPost}/>
       <PostList posts={posts} title="Список постов 1" />
     </div>
   );
