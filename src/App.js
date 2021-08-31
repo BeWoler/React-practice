2a101a2d9c6037cb97095dfeb71ec7a8fdfd4e4a
BeWoler <bewoler.del@gmail.com>
2021-08-31 11:41:24 +0300

    added PostList component with properties

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
 import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -10,6 +11,12 @@ function App() {
     { id: 2, title: "JavaScript 2", body: "Description" },
     { id: 3, title: "JavaScript 5", body: "Description" },
     { id: 4, title: "JavaScript 4", body: "Description" },
+  ]);
+  const [posts2, setPosts2] = useState([
+    { id: 1, title: "React", body: "Description React" },
+    { id: 2, title: "React 2", body: "Description React" },
+    { id: 3, title: "React 5", body: "Description React" },
+    { id: 4, title: "React 4", body: "Description React" },
   ]);
   return (
     <div className="App">
@@ -21,10 +28,8 @@ function App() {
         <h2>Class component</h2>
         <ClassCounter />
       </div> */}
-      <h1 style={{ textAlign: "center" }}>Список постов</h1>
-      {posts.map((post) => {
-        return <PostItem post={post} key={post.id} />;
-      })}
+      <PostList posts={posts} title="Список постов 1" />
+      <PostList posts={posts2} title="Список постов 2" />
     </div>
   );
 }
