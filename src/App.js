4396e5c35a1ec6f50684bc4995c6454f9ebcfa8c
BeWoler <bewoler.del@gmail.com>
2021-08-31 11:33:19 +0300

    added posts with props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
+import { useState } from "react";
 import "./App.css";
 import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
+import PostItem from "./components/PostItem";
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "JavaScript", body: "Description" },
+    { id: 2, title: "JavaScript 2", body: "Description" },
+    { id: 3, title: "JavaScript 5", body: "Description" },
+    { id: 4, title: "JavaScript 4", body: "Description" },
+  ]);
   return (
     <div className="App">
-      <div className="component__box">
+      {/* <div className="component__box">
         <h2>Functional component</h2>
         <Counter />
       </div>
       <div className="component__box">
         <h2>Class component</h2>
         <ClassCounter />
-      </div>
+      </div> */}
+      <h1 style={{ textAlign: "center" }}>Список постов</h1>
+      {posts.map((post) => {
+        return <PostItem post={post} key={post.id} />;
+      })}
     </div>
   );
 }
